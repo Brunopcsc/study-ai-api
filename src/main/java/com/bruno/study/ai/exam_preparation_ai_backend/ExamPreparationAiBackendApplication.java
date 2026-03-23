f815302a4ac7850717b258e23f5d38b953b7aa98
Bruno Coelho <brunopc15@gmail.com>
2026-03-23 14:29:59 -0300

    feat: initial spring boot setup with docker and flyway

---
+++ src/main/java/com/bruno/study/ai/exam_preparation_ai_backend/ExamPreparationAiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bruno.study.ai.exam_preparation_ai_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamPreparationAiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamPreparationAiBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bruno/study/ai/exam_preparation_ai_backend/ExamPreparationAiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bruno.study.ai.exam_preparation_ai_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamPreparationAiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
